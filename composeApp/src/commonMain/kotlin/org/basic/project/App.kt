706bfdf18eb2b9938f6f9aa528e174889247fbcc
Dino Meschini <francodmsoloing@gmail.com>
2025-08-26 18:49:43 -0300

    ultimo push

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -3,12 +3,15 @@ package org.basic.project
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,8 +19,15 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -27,40 +37,147 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
-        MaterialTheme {
-            var name : String by remember { mutableStateOf("") }
-            var surname : String by remember { mutableStateOf("") }
-            Column (modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding().fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
+        Navigator(
+            screen = MainScreen()
+        )
+    }
+}
 
+class MainScreen: Screen {
+
+    @Composable
+    override fun Content() {
+
+        val navigator: Navigator = LocalNavigator.currentOrThrow
+        var name: String by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = name,
+                onValueChange = {
+                    name = it
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
             )
-            {
-                TextField(
-                    value = name,
-                    onValueChange = {name = it},
-                )
-                Spacer(modifier = Modifier.height(40.dp))
-                AnimatedVisibility(name.isNotEmpty()){
-                    Text(
-                        text = "Version animada $name",
-                        fontSize = 24.sp
-                    )
-                }
-                Spacer(modifier = Modifier.height(40.dp))
-                TextField(
-                    value = surname,
-                    onValueChange = {surname = it},
-                )
-                AnimatedVisibility(surname.isNotEmpty()){
-                    Text(
-                        text = "Version animada $surname",
-                        fontSize = 24.sp
-                    )
-                }
 
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AnimatedVisibility(name.isNotEmpty()) {
+                Text(
+                    text = "Versión animada $name",
+                    fontSize = 24.sp
+                )
             }
 
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+                onClick = {
+                    navigator.push(SecondScreen())
+
+                }
+            ) {
+                Text(
+                    text = "Navegando"
+                )
+            }
+            Button(
+                onClick = {
+                    navigator.push(ThirdScreen())
+
+                }
+            ) {
+                Text(
+                    text = "Tercera"
+                )
+            }
+        }
+
+    }
+}
+
+class SecondScreen: Screen {
+
+    @Composable
+    override fun Content() {
+
+        val navigator: Navigator = LocalNavigator.currentOrThrow
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Yellow),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Segunda pantalla",
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+            )
+
+            Button(
+                onClick = {
+                    navigator.pop()
+                }
+            ) {
+                Text(
+                    text = "Vamos para atrás"
+                )
+            }
         }
     }
+
+}
+
+class ThirdScreen: Screen {
+
+    @Composable
+    override fun Content() {
+
+        val navigator: Navigator = LocalNavigator.currentOrThrow
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Magenta),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Tercera pantalla",
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+            )
+
+            Button(
+                onClick = {
+                    navigator.pop()
+                }
+            ) {
+                Text(
+                    text = "Vamos para atrás"
+                )
+            }
+        }
+    }
+
 }
