b4d051341dd596ceec91f5fe81659de7d5315090
Dino Meschini <francodmsoloing@gmail.com>
2025-08-26 17:14:37 -0300

    primer push oficial

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -22,5 +22,24 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
+        MaterialTheme {
+            Column (modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding().fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            )
+            {
+                Text(
+                    text = "Hola mundo!!!"
+                )
+                Text(
+                    text = "Programación 2"
+                )
+                Text(
+                    text = "Android Studio"
+                )
+            }
+
+        }
     }
 }
